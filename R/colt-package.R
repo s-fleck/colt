a9a32a8ffc4750c127048ae5903966560345ad7b
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-08-18 14:04:46 +0200

    added package doc

--- R/colt-package.R
+++ R/colt-package.R
@@ -1,19 +1,2 @@
 #' @keywords internal
 "_PACKAGE"
-
-
-.onLoad <- function(...) {
-  op <- options()
-
-  op.testthis <- list(
-    colt.theme = '_'
-  )
-
-  toset <- !(names(op.testthis) %in% names(op))
-  if(any(toset)) options(op.testthis[toset])
-
-  invisible()
-}
-
-
-
