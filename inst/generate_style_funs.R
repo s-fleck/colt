67fd243394882fc9d15627626a53fcb2d035eeb6
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-08-18 22:24:23 +0200

    document and load on run

--- inst/generate_style_funs.R
+++ inst/generate_style_funs.R
@@ -1,5 +1,8 @@
 library(glue)
 library(rprojroot)
+devtools::load_all()
+
+x <- colt_theme()
 
 funs <- glue::glue('
     #\' @export
@@ -9,7 +12,7 @@ funs <- glue::glue('
 
 
   '
-  , name = names(options("colt.theme")[[1]])
+  , name = names(x)
 )
 
 funs <- c("# This file is generated automatically by inst/generate_style_funs.R.\n# Do not edit manually.\n\n", funs)
@@ -18,3 +21,6 @@ writeLines(
   funs,
   rprojroot::find_package_root_file("R", "styles.R")
 )
+
+devtools::document()
+devtools::load_all()
