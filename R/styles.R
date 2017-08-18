76a57df85857435229fa347a74f35fd47c5bd2d0
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-08-18 22:21:50 +0200

    add (automatically generated) styles for ease of download from github

---
+++ R/styles.R
@@ -0,0 +1,226 @@
+# This file is generated automatically by inst/generate_style_funs.R.
+# Do not edit manually.
+
+
+#' @export
+default <- function(x, fun = options("colt.theme")[[1]]$default){
+  fun(x)
+}
+
+
+#' @export
+chr <- function(x, fun = options("colt.theme")[[1]]$chr){
+  fun(x)
+}
+
+
+#' @export
+chr_ <- function(x, fun = options("colt.theme")[[1]]$chr_){
+  fun(x)
+}
+
+
+#' @export
+chr2 <- function(x, fun = options("colt.theme")[[1]]$chr2){
+  fun(x)
+}
+
+
+#' @export
+chr2_ <- function(x, fun = options("colt.theme")[[1]]$chr2_){
+  fun(x)
+}
+
+
+#' @export
+chr3 <- function(x, fun = options("colt.theme")[[1]]$chr3){
+  fun(x)
+}
+
+
+#' @export
+chr3_ <- function(x, fun = options("colt.theme")[[1]]$chr3_){
+  fun(x)
+}
+
+
+#' @export
+yes <- function(x, fun = options("colt.theme")[[1]]$yes){
+  fun(x)
+}
+
+
+#' @export
+no <- function(x, fun = options("colt.theme")[[1]]$no){
+  fun(x)
+}
+
+
+#' @export
+na <- function(x, fun = options("colt.theme")[[1]]$na){
+  fun(x)
+}
+
+
+#' @export
+null <- function(x, fun = options("colt.theme")[[1]]$null){
+  fun(x)
+}
+
+
+#' @export
+inf <- function(x, fun = options("colt.theme")[[1]]$inf){
+  fun(x)
+}
+
+
+#' @export
+num <- function(x, fun = options("colt.theme")[[1]]$num){
+  fun(x)
+}
+
+
+#' @export
+num_ <- function(x, fun = options("colt.theme")[[1]]$num_){
+  fun(x)
+}
+
+
+#' @export
+num2 <- function(x, fun = options("colt.theme")[[1]]$num2){
+  fun(x)
+}
+
+
+#' @export
+num2_ <- function(x, fun = options("colt.theme")[[1]]$num2_){
+  fun(x)
+}
+
+
+#' @export
+num3 <- function(x, fun = options("colt.theme")[[1]]$num3){
+  fun(x)
+}
+
+
+#' @export
+num3_ <- function(x, fun = options("colt.theme")[[1]]$num3_){
+  fun(x)
+}
+
+
+#' @export
+hdr <- function(x, fun = options("colt.theme")[[1]]$hdr){
+  fun(x)
+}
+
+
+#' @export
+hdr2 <- function(x, fun = options("colt.theme")[[1]]$hdr2){
+  fun(x)
+}
+
+
+#' @export
+hdr3 <- function(x, fun = options("colt.theme")[[1]]$hdr3){
+  fun(x)
+}
+
+
+#' @export
+bullet <- function(x, fun = options("colt.theme")[[1]]$bullet){
+  fun(x)
+}
+
+
+#' @export
+bullet2 <- function(x, fun = options("colt.theme")[[1]]$bullet2){
+  fun(x)
+}
+
+
+#' @export
+bullet3 <- function(x, fun = options("colt.theme")[[1]]$bullet3){
+  fun(x)
+}
+
+
+#' @export
+chk <- function(x, fun = options("colt.theme")[[1]]$chk){
+  fun(x)
+}
+
+
+#' @export
+chk2 <- function(x, fun = options("colt.theme")[[1]]$chk2){
+  fun(x)
+}
+
+
+#' @export
+chk3 <- function(x, fun = options("colt.theme")[[1]]$chk3){
+  fun(x)
+}
+
+
+#' @export
+radio <- function(x, fun = options("colt.theme")[[1]]$radio){
+  fun(x)
+}
+
+
+#' @export
+radio2 <- function(x, fun = options("colt.theme")[[1]]$radio2){
+  fun(x)
+}
+
+
+#' @export
+log_info <- function(x, fun = options("colt.theme")[[1]]$log_info){
+  fun(x)
+}
+
+
+#' @export
+log_trace <- function(x, fun = options("colt.theme")[[1]]$log_trace){
+  fun(x)
+}
+
+
+#' @export
+log_debug <- function(x, fun = options("colt.theme")[[1]]$log_debug){
+  fun(x)
+}
+
+
+#' @export
+log_warn <- function(x, fun = options("colt.theme")[[1]]$log_warn){
+  fun(x)
+}
+
+
+#' @export
+log_error <- function(x, fun = options("colt.theme")[[1]]$log_error){
+  fun(x)
+}
+
+
+#' @export
+log_fatal <- function(x, fun = options("colt.theme")[[1]]$log_fatal){
+  fun(x)
+}
+
+
+#' @export
+emph <- function(x, fun = options("colt.theme")[[1]]$emph){
+  fun(x)
+}
+
+
+#' @export
+emph2 <- function(x, fun = options("colt.theme")[[1]]$emph2){
+  fun(x)
+}
+
+
