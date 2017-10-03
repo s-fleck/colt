31baf1ba50725dfb3f783ef0a155208ea1c4dc09
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-10-03 19:58:30 +0200

    display packageStartupMessage only on attach(not on load)

--- R/zzz.R
+++ R/zzz.R
@@ -1,27 +1,16 @@
 # Make data available
 data(clt_theme_light, clt_theme_dark, envir=environment())
 
+
+
+
 .onLoad <- function(...) {
   op <- options()
 
-
-  if (requireNamespace("rstudioapi")){
-    use_dark_theme <- tryCatch(
-      rstudioapi::getThemeInfo()[["dark"]],
-      error = function(e) NA,
-      silent = TRUE
-    )
-  } else {
-    use_dark_theme <- NA
-  }
+  use_dark_theme <- is_console_dark()
 
   if (is.na(use_dark_theme)){
     op.default <- list(colt.theme = colt_theme())
-    packageStartupMessage(paste(
-      "Colt could not detect the console color theme. If you want to use terminal",
-      "colors, please use colt::set_colt_theme(colt::clt_theme_light) or",
-      "colt::set_colt_theme(colt::clt_theme_dark) to set an appropriate color theme.")
-    )
   } else if (use_dark_theme) {
     op.default <- list(colt.theme = clt_theme_dark)
   } else {
@@ -33,4 +22,32 @@ data(clt_theme_light, clt_theme_dark, envir=environment())
   if(any(toset)) options(op.default[toset])
 
   invisible()
+}
+
+
+
+
+.onAttach <- function(...){
+  if (is.na(is_console_dark())){
+    packageStartupMessage(paste(
+      "Colt could not detect the console color theme. If you want to use terminal",
+      "colors, please use colt::set_colt_theme(colt::clt_theme_light) or",
+      "colt::set_colt_theme(colt::clt_theme_dark) to set an appropriate color theme.")
+    )
+  }
+}
+
+
+
+
+is_console_dark <- function(){
+  if (requireNamespace("rstudioapi")){
+    use_dark_theme <- tryCatch(
+      rstudioapi::getThemeInfo()[["dark"]],
+      error = function(e) NA,
+      silent = TRUE
+    )
+  } else {
+    use_dark_theme <- NA
+  }
 }
