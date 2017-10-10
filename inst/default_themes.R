1c1964aed91ceb7e06edec57f6ee67cd1daf9e3d
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-10-10 06:05:27 +0200

    minimal changes to default themes

--- inst/default_themes.R
+++ inst/default_themes.R
@@ -17,7 +17,7 @@ clt_theme_light <- colt_theme(
   clt_num_subtle = crayon::silver,
 
   clt_bg = identity,
-  clt_bg_subtle = crayon::make_style(grDevices::grey(0.9), bg = TRUE, grey = TRUE),
+  clt_bg_subtle = crayon::make_style(grDevices::grey(0.6), bg = TRUE, grey = TRUE),
   clt_bg_accent = crayon::make_style("#F3F315", bg = TRUE),
 
   clt_na = crayon::magenta,
@@ -47,15 +47,17 @@ clt_theme_dark <- colt_theme(
   clt_chr_subtle = crayon::silver,
 
   clt_true = crayon::green,
-  clt_false = crayon::red,
+  clt_false = crayon::make_style("#E52B50", colors = 256),
   clt_maybe = crayon::yellow,
+
+
 
   clt_num = crayon::white,
   clt_num_accent = crayon::blue,
   clt_num_subtle = crayon::silver,
 
   clt_bg = identity,
-  clt_bg_subtle = crayon::make_style("#333333", bg = TRUE, grey = TRUE, colors = 256),
+  clt_bg_subtle = crayon::make_style(grDevices::grey(0.4), bg = TRUE, grey = TRUE),
   clt_bg_accent = crayon::make_style("#3B3B6D", bg = TRUE, colors = 256),
 
   clt_na = crayon::magenta,
